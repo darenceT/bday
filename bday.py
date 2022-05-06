af1d95b7ed99d5652fe2bec2b257ffb216a2a5a2
darenT <darence.thong@gmail.com@users.noreply.github.com>
2022-05-06 14:45:03 -0700

    stage 1

--- bday.py
+++ bday.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "hello"
+def home():
+    return render_template("home.html", body="Custom body")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
